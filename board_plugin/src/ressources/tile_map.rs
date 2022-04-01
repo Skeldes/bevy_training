adba3d775bc677144a574f4d579602856a2a40df
Skelde <thomas.feuilletin1@gmail.com>
2022-04-01 12:38:08 +0200

    editing tile_map.rs

--- board_plugin/src/ressources/tile_map.rs
+++ board_plugin/src/ressources/tile_map.rs
@@ -22,4 +22,54 @@ impl TileMap {
             map,
         }
     }
+
+    #[cfg(featurte = "debug")]
+    pub fn console_output(&self) -> String {
+        let mut bufffer = format!(
+            "Map ({},{}) with {} bombs:\n",
+            self.width, self.height, self.bomb_count
+        );
+        let line: String = (0..=(self.width+1)).into_iter().map(|_| '-').collect();
+        buffer = format!("{}{}\n", buffer, line);
+        for line in self.iter().rev(){
+            buffer = format!("{}|", buffer);
+            for tile in line.iter() {
+                buffer = format!("{}{}", buffer, tile.console_output());
+            }
+            buffer = format!("{}|\n",buffer);
+        }
+        buffer = format!("{}{}", buffer, line)
+    }
+
+
+
+    //Getter
+    pub fn width(&self) -> u16 {
+        self.width
+    }
+
+    pub fn height(&self) -> u16 {
+        self.height
+    }
+
+    pub fn bomb_count(&self) -> u16 {
+        self.bomb_count
+    }
+}
+
+
+
+impl Deref for TileMap {
+    type Target = Vec<Vec<Tile>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.map
+    }
+}
+
+
+impl DerefMut for TileMap {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.map
+    }
 }
